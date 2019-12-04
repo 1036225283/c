215816d75a9167e4662de125d7e11832424acac0
1036225283 <id512@126.com>
2019-12-04 12:22:32 +0800

    typeof struct

--- user/show.c
+++ user/show.c
@@ -9,22 +9,24 @@ struct Student
     char phone[13];
 };
 
+typedef struct Student Student;
+
 int max(int x, int y)
 {
     return x > y ? x : y;
 }
 
-struct Student createStudent()
+Student createStudent()
 {
-    struct Student stu;
+    Student stu;
     stu.nAge = 12;
     stu.nSex = 1;
     return stu;
 };
 
-struct Student *createStudent1()
+Student *createStudent1()
 {
-    struct Student *stu = (struct Student *)malloc(sizeof(struct Student));
+    Student *stu = (Student *)malloc(sizeof(Student));
     stu->nAge = 22;
     stu->nSex = 2;
     return stu;
@@ -35,10 +37,10 @@ void show()
     int nAge = 10;
     int *test = &nAge;
     int (*fMax)(int, int) = &max; // &可以省略
-    struct Student stu = createStudent();
+    Student stu = createStudent();
     printf("student 0 age  = : %d\n", stu.nAge);
 
-    struct Student *stu1 = createStudent1();
+    Student *stu1 = createStudent1();
     printf("student 1 age  = : %d\n", stu1->nAge);
 
     printf("max 12 and 13  = : %d\n", fMax(12, 13));
